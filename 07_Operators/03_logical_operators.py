46bbde6f64df8002f005ede602451626a984b10d
unpredictable-arcane <arcane.anuj@gmail.com>
2026-01-30 23:46:06 +0530

    Add 07_Operators/03_logical_operators

---
+++ 07_Operators/03_logical_operators.py
@@ -0,0 +1,18 @@
+c = True
+d = False
+
+#Logical operators
+print(False and True)
+print(True and False)
+print(True and True)
+print(False and False)
+
+print("For Or Operator...")
+print(True or True)
+print(False or False)
+print(True or False)
+print(False or True)
+
+print("Not Operator")
+print(not(True))
+print(not(False))
