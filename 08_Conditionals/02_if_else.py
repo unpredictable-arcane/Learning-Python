37eac18512efff33e660167706d66c2ec8cfbe4e
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-04 13:37:19 +0530

    Add 08_Conditionals/02_if_else.py

---
+++ 08_Conditionals/02_if_else.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can drive a car.")   
+else:
+    print("You cannot drive a car yet.")
+print("Thank you for your time.")
+print("End of the program.")
