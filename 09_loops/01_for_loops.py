d8ac2a085412a2115f3ec20c13ba8694b97e5e67
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-09 21:39:53 +0530

    Add for loop example

---
+++ 09_loops/01_for_loops.py
@@ -0,0 +1,7 @@
+for i in range(17):
+    print(i)    
+# This code will print the numbers 0 to 4, one on each line.
+# The range function generates a sequence of numbers starting from 0 up to, but not including
+
+for i in range(1,11):
+    print("5 x",i, "=", 5*i)
