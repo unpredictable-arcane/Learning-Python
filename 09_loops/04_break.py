adbbd7b3d5bdfbe8dc5bf337c399fe9a30fe826f
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-11 12:41:42 +0530

    Add break statement example

---
+++ 09_loops/04_break.py
@@ -0,0 +1,5 @@
+for i in range (0, 21):
+    print(i)
+    if i == 10:
+        break # This code will print the numbers 0 to 20, one on each line.
+# The loop will stop when i reaches 10 due to the break statement.
