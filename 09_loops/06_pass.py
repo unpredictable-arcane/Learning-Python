e9f117d73197b15e750e0958fda2365f31e3ba14
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-11 12:40:55 +0530

    Add pass statement example

---
+++ 09_loops/06_pass.py
@@ -0,0 +1,5 @@
+i = 3 
+
+if i == 12:
+    pass  # This code does nothing and is a placeholder for future code.
+print("End of Program")  # This will always execute regardless of the pass statement.
