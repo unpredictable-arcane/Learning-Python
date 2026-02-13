5631efe26f49203618885501b55ff44a8e556c2e
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-14 02:45:11 +0530

    Add indexing example

---
+++ 10_Strings/02_indexing.py
@@ -0,0 +1,15 @@
+name = "Anuj"
+
+# name = "A  n  u  j"
+#         0  1  2   3
+#        -4 -3  -2  -1
+# print (name[0])
+# print (name[1])
+# print (name[2])
+# print (name[3])
+
+print(name[-1])  # A
+print(name[-2])  # n
+print(name[-3])  # u    
+print(name[-4])  # j
+# print(name[-5])  # IndexError: string index out of range
