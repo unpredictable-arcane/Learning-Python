2302e6940ae01f15d5d43a8a8a27a7e1bb0b8beb
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-19 17:06:10 +0530

    Add functions example

---
+++ 11_Functions/01_functions.py
@@ -0,0 +1,24 @@
+# a = 4 
+# b = 2
+# c = 1
+
+# average = (a + b + c) / 3
+# print(average)
+
+# a1 = 5
+# b1 = 3
+# c1 = 2
+# average1 = (a1 + b1 + c1) / 3
+# print(average1)
+
+def average(a, b, c):
+    d = (a + b + c) / 3.0
+    # print(d)
+    return d
+
+o1 = average(3, 5, 1)
+
+o2 = average(4, 2, 1)
+
+print(o1)
+print(o2)
