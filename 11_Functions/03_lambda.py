cb2ed2668c74b0d44b243d588a33ee27d424aa54
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-24 12:33:41 +0530

    Add lambda function example

---
+++ 11_Functions/03_lambda.py
@@ -0,0 +1,19 @@
+square = lambda x: x * x
+'''
+As good as writing
+def square(x):
+    return x * x
+'''
+
+
+
+sum = lambda a, b: a + b
+'''
+ As good as writing 
+d ef sum(a, b):
+    return a + b
+'''
+
+
+print(square(5))
+print(sum(3, 4))
