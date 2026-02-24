524bd06a4a00c023a52b5ce9e6e67795ee2a9212
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-24 12:36:18 +0530

    Add modules example

---
+++ 11_Functions/05_modules.py
@@ -0,0 +1,11 @@
+# Two types of modules in Python are:
+# - Built-in modules
+# - External modules
+# List of all the built in Modules: http://docs.python.org/3/tutorial/modules.html
+
+import math 
+import os
+import mymodule
+
+print(math.sqrt(16))
+mymodule.hello()
