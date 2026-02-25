a06ddfc9b9b242931e95e41916a7931c521e8518
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-26 04:32:07 +0530

    Add docstrings

---
+++ 11_Functions/08_docstrings.py
@@ -0,0 +1,6 @@
+def sum(a, b):
+    '''This will sum two numbers a and b and return the result.'''
+    c = a + b
+    return c
+
+print(sum.__doc__)  # This will print the docstring of the function
