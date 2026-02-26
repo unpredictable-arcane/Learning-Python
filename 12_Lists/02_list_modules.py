e0e37b35b6801977d600618126df7bbb5944877a
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-27 03:47:55 +0530

    Add list modules code

---
+++ 12_Lists/02_list_modules.py
@@ -0,0 +1,10 @@
+marks = [5, 3 , 11, 8, 2, 9]
+extra_marks = [49, 62, 44]
+
+print(marks)
+# marks.append(7) # Adding 7 to the end of the list
+# print(marks)
+# marks.pop(5) # Removing the element at index 5
+marks.extend(extra_marks) # Adding extra_marks to the end of marks
+
+print(marks) 
