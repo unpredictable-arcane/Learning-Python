29b092293b54e33a8da5ff7a8005f3c804d66424
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-27 03:47:05 +0530

    Add list practise code

---
+++ 12_Lists/01_lists.py
@@ -0,0 +1,6 @@
+marks = [54, 23, 45, 67, 89, 12, 34]
+mixed = [1, 2, 3, 'apple', 'ball', 'c']
+
+print(marks[0])
+print(marks[1:4])
+print(mixed[6]) # This will raise an IndexError since there is no index 6 in mixed
