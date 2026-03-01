5916f5e7ac3476c518271b323031cac160d1a089
unpredictable-arcane <arcane.anuj@gmail.com>
2026-03-01 16:12:27 +0530

    Add dictionary methods

---
+++ 15_dictionaries/02_dict_methods.py
@@ -0,0 +1,7 @@
+marks = {"harry": 85, "sally": 90, "bob": 78}
+
+print(marks.keys())
+print(marks.values())
+# marks.clear()  # Clear all items in the dictionary
+marks.pop("harry")  # Remove a key-value pair by key
+print(marks) 
