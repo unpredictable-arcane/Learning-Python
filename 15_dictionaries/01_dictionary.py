ddddf0bf2abe3ed7d0738cab536ce23c16165274
unpredictable-arcane <arcane.anuj@gmail.com>
2026-03-01 16:11:31 +0530

    Add dictionary operations

---
+++ 15_dictionaries/01_dictionary.py
@@ -0,0 +1,6 @@
+marks = {"harry": 85, "sally": 90, "bob": 78}
+
+# print(marks, type(marks))
+print(marks["sally"])  # Accessing value by key
+marks["bob"] = 82  # Updating value for an existing key
+print(marks) 
