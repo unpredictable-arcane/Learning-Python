75dfb9b22e0632ae628f6ab40c3370fbe2d36bbe
unpredictable-arcane <arcane.anuj@gmail.com>
2026-05-16 08:45:42 +0530

    Add data types learning

---
+++ 03_Variables/02-data-types.py
@@ -0,0 +1,30 @@
+# Python supports several built-in data types:
+
+#     Integers (int): Whole numbers (e.g., 10, 0).
+#     Floats (float): Decimal numbers (e.g., 3.142, -0.001).
+#     Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+#     Booleans (bool): Represents True or False.
+#     Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+#     Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+#     Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+#     Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+
+age = 3 
+print(age)
+
+print(type(age))
+
+
+cgpa = 8.2
+print(cgpa)
+print(type(cgpa))
+
+
+name = "Harry"
+print(name)
+print(type(name))
+
+
+is_completed = True # can also be False
+print(is_completed)
+print(type(is_completed))
