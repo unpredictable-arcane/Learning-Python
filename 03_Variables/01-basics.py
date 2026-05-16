8ed25e643fce32b1166e04a36a2913de19febfa5
unpredictable-arcane <arcane.anuj@gmail.com>
2026-05-16 08:44:03 +0530

    Add basic variable learning

---
+++ 03_Variables/01-basics.py
@@ -0,0 +1,18 @@
+# In Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assignment operator (=).
+
+age = 34 # integer
+name = "Harry" # string
+cgpa = 4.55 # float
+
+# Rule of defining a variable in Python  
+# Variable names must start with a letter (a-z, A-Z) or an underscore (_).
+# They can contain letters, numbers, and underscores.
+# Variable names are case-sensitive (age and Age are different).
+# Avoid using Python keywords (e.g., if, for, while) as variable names.
+
+# 34age = 4 # Invalid because variable cannot start with a number
+age = 32 # Valid because variable can start with a number 
+# a$$ge = 45 # Invlaid because variables cannot contain special characters other than _
+__age = 34
+__nice_45 = 34
+a_b_c_7 = "Sam"
