8230f257905c9c098ab75a9cfeb3a8d756e7062a
unpredictable-arcane <arcane.anuj@gmail.com>
2026-05-27 19:45:01 +0530

    Add Flask Jinja comments loop python file

---
+++ Flask/flask_jinja_comments_loop/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    marks = {
+        "pikachu": 88,
+        "Bulbasaur": 74,
+        "Charmander": 79,
+        "Squirtle": 68,
+        "Archaludon": 95,
+        "Mew": 99,
+        "Mewtwo": 98
+    }
+    return render_template("index.html", marks=marks)
+
+app.run(debug=True)
+
+#add jinja snippet from extensions, and also add the emmet
