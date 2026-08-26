b8ddf7e998d2ce36fdc732175afd315217ae85e3
unpredictable-arcane <arcane.anuj@gmail.com>
2026-08-26 21:20:36 +0530

    Add numbers file

---
+++ 21_Extra/3-Milk.py
@@ -0,0 +1,34 @@
+# Integer
+
+black_tea_grams = 37
+ginger_grams = 98
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea is {total_grams}")
+
+remaing_tea = black_tea_grams - ginger_grams
+print(f"Total grams of remaining tea is {remaing_tea}")
+
+milk_litres = 2
+servings = 6
+milk_per_serving = milk_litres / servings
+print(f"Milk per serving is {milk_per_serving}")
+
+total_tea_bags = 48
+pots = 11
+bags_per_pot = total_tea_bags // pots
+print(f"While tea bags per pot: {bags_per_pot}")
+
+total_cadamom_pods = 37
+pods_per_cup = 4
+leftover_pods = total_cadamom_pods % pods_per_cup
+print(f"Leftover C pods {leftover_pods}")
+
+base_flavor_strength = 55
+scale_factor = 4
+powerful_falvour = base_flavor_strength ** scale_factor
+print(f"Scaled flavour strenght {powerful_falvour}")
+# 2 * 2 * 2
+
+# total_tea_leaves_harvested = 9_336_000_000
+# print(f"tea leaves: {total_tea_leaves_harvested}")
