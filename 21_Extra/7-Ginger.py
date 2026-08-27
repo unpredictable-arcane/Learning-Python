dc790ec379cdf1460b916810a51d1ed5f3bb1e7e
unpredictable-arcane <arcane.anuj@gmail.com>
2026-08-27 23:24:04 +0530

    add tuples testing file

---
+++ 21_Extra/7-Ginger.py
@@ -0,0 +1,12 @@
+masala_spices = ("cardamom", "cloves", "cinnamon")
+
+(spice1, spice2, spice3) = masala_spices
+
+print(f"Main masala spices: {spice1}, {spice2}, {spice3}")
+
+ginger_ratio, cardamom_ratio = 2, 1
+print(f"Ratio is G :{ginger_ratio} and C: {cardamom_ratio}")
+ginger_ratio, cardamom_ratio = cardamom_ratio, ginger_ratio
+print(f"Ratio is G :{ginger_ratio} and C: {cardamom_ratio}")
+
+print(f"Is cinnamon in the masala spices ? {'cinnamon' in masala_spices}")
